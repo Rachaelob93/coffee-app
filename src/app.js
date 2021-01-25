2b2b5be7d62496e57b2a21eec1ca37f4d08930ac
Rachael O'Brien <rachaelob93@gmail.com>
2021-01-25 17:38:22 +0000

    modules installed and imported

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const inquirer = require("inquirer");
+const figlet = require("figlet");
+const chalk = require("chalk");
+
+const main = () = {
+    console.log(chalk.greenBright("Hello there!"));
+}
+main();
+const app = async () => {
+
+}
